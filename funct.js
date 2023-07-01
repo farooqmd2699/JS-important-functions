bf87a2fa5b8116a51f045891b2b882940e8ca210
farooqmd2699 <farooqmd9962@gmail.com>
2023-07-01 12:09:26 +0530

    Update funct.js

--- funct.js
+++ funct.js
@@ -23,3 +23,24 @@
         replace(/\n +/, "\n"); // Removes spaces after newlines
         return;
     }
+
+//to take input phone number only numbers will not accept 0
+
+ jQuery(document).ready(function () {
+      jQuery("#input-custom-field4").keypress(function (e) {
+         var length = jQuery(this).val().length;
+       if(length > 9) {
+            return false;
+       } else if(e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+            return false;
+       } else if((length == 0) && (e.which == 48)) {
+            return false;
+       }
+      });
+    });
+
+
+///
+                  
+                   
+			
